3874865bd7194fdbbead0ef8168653e5deac5965
Veronika Smoliak <a.smolyak@cardsmobile.ru>
2022-11-18 23:57:27 +0300

    task2 added

---
+++ task2/Program.cs
@@ -0,0 +1,38 @@
+﻿var Array = CreateArrayWithRandomNumbers();
+Print(Array);
+CountSumOddPositions(Array);
+
+
+int[] CreateArrayWithRandomNumbers(int numbers = 4, int Min = -100, int Max = 100)
+{
+
+    int[] Array = new int[numbers];
+
+    Random random = new Random();
+
+    for (int i = 0; i < numbers; i++)
+    {
+
+        Array[i] = random.Next(Min,Max);
+    }
+
+    return Array;
+}
+
+void Print(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" {array[i]}");
+    }
+    Console.WriteLine();
+}
+
+void CountSumOddPositions(int[] Array) {
+int Sum = 0;
+for (int i = 0; i < Array.Length; i+=2)
+{
+        Sum = Sum + Array [i];
+}
+Console.WriteLine($"Сумма чисел на нечетных позициях = {Sum}");
+}
